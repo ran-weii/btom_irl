7b2718a60d2509762b8475423d8a0d25059035a3
HFML <rw422@tamu.edu>
2023-03-10 23:34:57 -0600

    update modified rambo
    replace rambo dynamics model with new EnsembleDynamics

--- src/agents/rambo.py
+++ src/agents/rambo.py
@@ -12,10 +12,11 @@ class RAMBO(MBPO):
     """ Robust adversarial model-based offline policy optimization """
     def __init__(
         self, 
+        reward,
+        dynamics,
         obs_dim, 
         act_dim, 
         act_lim, 
-        ensemble_dim, 
         hidden_dim, 
         num_hidden, 
         activation, 
@@ -23,21 +24,17 @@ class RAMBO(MBPO):
         beta=0.2, 
         polyak=0.995, 
         tune_beta=True,
-        clip_lv=False, 
-        rwd_clip_max=10., 
         adv_penalty=10., 
         norm_advantage=False,
         update_critic_adv=False,
         buffer_size=1e6, 
-        model_retain_epochs=5,
         batch_size=200, 
         rollout_batch_size=10000, 
         rollout_min_steps=1, 
         rollout_max_steps=10, 
         rollout_min_epoch=20, 
         rollout_max_epoch=100, 
-        topk=5,
-        termination_fn=None, 
+        model_retain_epochs=5,
         real_ratio=0.05, 
         eval_ratio=0.2,
         m_steps=100, 
@@ -45,16 +42,16 @@ class RAMBO(MBPO):
         lr_a=1e-4, 
         lr_c=3e-4, 
         lr_m=3e-4, 
-        decay=None, 
         grad_clip=None,
         device=torch.device("cpu")
         ):
         """
         Args:
+            reward (EnsembleDynamics): reward function as an EnsembleDynamics object
+            dynamics (EnsembleDynamics): transition function as an EnsembleDynamics object
             obs_dim (int): observation dimension
             act_dim (int): action dimension
             act_lim (torch.tensor): action limits
-            ensemble_dim (int): number of ensemble models
             hidden_dim (int): value network hidden dim
             num_hidden (int): value network hidden layers
             activation (str): value network activation
@@ -62,21 +59,17 @@ class RAMBO(MBPO):
             beta (float, optional): softmax temperature. Default=0.2
             polyak (float, optional): target network polyak averaging factor. Default=0.995
             tune_beta (bool, optional): whether to automatically tune temperature. Default=True
-            clip_lv (bool, optional): whether to soft clip observation log variance. Default=False
-            rwd_clip_max (float, optional): clip reward max value. Default=10.
             adv_penalty (float, optional): model advantage penalty. Default=3e-4.
             norm_advantage (bool, optional): whether to normalize advantage. Default=False
             update_adv_critic (bool, optional): whether to udpate critic during model update. Default=False
             buffer_size (int, optional): replay buffer size. Default=1e6
-            model_retain_epochs (int, optional): number of epochs to keep model samples. Default=5
             batch_size (int, optional): actor and critic batch size. Default=100
             rollout_batch_size (int, optional): model_rollout batch size. Default=10000
             rollout_min_steps (int, optional): initial model rollout steps. Default=1
             rollout_max_steps (int, optional): maximum model rollout steps. Default=10
             rollout_min_epoch (int, optional): epoch to start increasing rollout length. Default=20
             rollout_max_epoch (int, optional): epoch to stop increasing rollout length. Default=100
-            topk (int, optional): top k models to perform rollout. Default=5
-            termination_fn (func, optional): termination function to output rollout done. Default=None
+            model_retain_epochs (int, optional): number of epochs to keep model samples. Default=5
             real_ratio (float, optional): ratio of real samples for policy training. Default=0.05
             eval_ratio (float, optional): ratio of real samples for model evaluation. Default=0.2
             m_steps (int, optional): model update steps per training step. Default=100
@@ -84,15 +77,15 @@ class RAMBO(MBPO):
             lr_a (float, optional): actor learning rate. Default=1e-4
             lr_c (float, optional): critic learning rate. Default=3e-4
             lr_m (float, optional): model learning rate. Default=3e-4
-            decay ([list, None], optional): weight decay for each dynamics and reward model layer. Default=None.
             grad_clip (float, optional): gradient clipping. Default=None
             device (optional): training device. Default=cpu
         """
         super().__init__(
-            obs_dim, act_dim, act_lim, ensemble_dim, hidden_dim, num_hidden, activation, 
-            gamma, beta, polyak, tune_beta, clip_lv, rwd_clip_max, False, buffer_size, model_retain_epochs, batch_size, 
-            rollout_batch_size, rollout_min_steps, rollout_max_steps, rollout_min_epoch, rollout_max_epoch, topk,
-            termination_fn, real_ratio, eval_ratio, m_steps, a_steps, lr_a, lr_c, lr_m, decay, grad_clip, device
+            reward, dynamics, obs_dim, act_dim, act_lim, hidden_dim, num_hidden, activation, 
+            gamma, beta, polyak, tune_beta, False, buffer_size, batch_size, 
+            rollout_batch_size, rollout_min_steps, rollout_max_steps, 
+            rollout_min_epoch, rollout_max_epoch, model_retain_epochs,
+            real_ratio, eval_ratio, m_steps, a_steps, lr_a, lr_c, lr_m, grad_clip, device
         )
         self.adv_penalty = adv_penalty
         self.norm_advantage = norm_advantage
@@ -102,21 +95,14 @@ class RAMBO(MBPO):
             "actor_loss_avg", "beta_avg", "adv_loss_avg", "obs_mae", 
         ]
     
-    def compute_dynamics_adversarial_loss(self, obs, act, done):
+    def compute_dynamics_adversarial_loss(self, obs, act):
         # sample next obs and rwd
         with torch.no_grad():
-            obs_norm = normalize(obs, self.obs_mean, self.obs_variance)
-            rwd_norm = self.sample_reward_dist(obs_norm, act)
-            next_obs_norm = self.sample_transition_dist(obs_norm, act)
-            rwd = denormalize(rwd_norm, self.rwd_mean, self.rwd_variance)
-            next_obs = denormalize(next_obs_norm, self.obs_mean, self.obs_variance)
-
-        # compute model advantage
-        if self.termination_fn is not None:
-            done = self.termination_fn(
-                obs.cpu().data.numpy(), act.cpu().data.numpy(), next_obs.cpu().data.numpy()
-            )
-            done = torch.from_numpy(done).view(-1, 1).to(torch.float32).to(self.device)
+            rwd, _ = self.reward.step(obs, act)
+            next_obs, done = self.dynamics.step(obs, act)
+            rwd_norm = normalize(rwd, self.reward.out_mean, self.reward.out_variance)
+            obs_norm = normalize(obs, self.dynamics.out_mean, self.dynamics.out_variance)
+            next_obs_norm = normalize(next_obs, self.dynamics.out_mean, self.dynamics.out_variance)
         
         # compute advantage
         q_1, q_2 = self.critic(obs, act)
@@ -134,14 +120,10 @@ class RAMBO(MBPO):
                 advantage_norm = advantage
         
         # compute ensemble mixture log likelihood
-        logp_rwd = self.compute_reward_log_prob(obs_norm, act, rwd_norm).sum(-1)
-        logp_obs = self.compute_transition_log_prob(obs_norm, act, next_obs_norm).sum(-1)
-        logp_elites = torch.log(self.topk_dist.to(self.device) + 1e-6).unsqueeze(0)
-        logp_mix_rwd = torch.logsumexp(logp_rwd + logp_elites, dim=-1, keepdims=True)
-        logp_mix_obs = torch.logsumexp(logp_obs + logp_elites, dim=-1, keepdims=True)
+        logp_rwd = self.reward.compute_mixture_log_prob(obs_norm, act, rwd_norm)
+        logp_obs = self.dynamics.compute_mixture_log_prob(obs_norm, act, next_obs_norm)
+        adv_loss = torch.mean(advantage_norm * (logp_rwd + logp_obs))
 
-        adv_loss = torch.mean(advantage_norm * (logp_mix_rwd + logp_mix_obs))
-        
         # update model aware q loss
         with torch.no_grad():
             q_target_next_1, q_target_next_2 = self.critic_target(next_obs, next_act)
@@ -154,29 +136,30 @@ class RAMBO(MBPO):
         adv_q_loss = (q1_loss + q2_loss) / 2 
 
         stats = {
+            "v_next_mean": v_next.cpu().mean().data.item(),
             "adv_mean": advantage.cpu().mean().data.item(),
             "adv_std": advantage.cpu().std().data.item(),
-            "logp_rwd_mean": logp_mix_rwd.cpu().mean().data.item(),
-            "logp_rwd_std": logp_mix_rwd.cpu().std().data.item(),
-            "logp_obs_mean": logp_mix_obs.cpu().mean().data.item(),
-            "logp_obs_std": logp_mix_obs.cpu().std().data.item(),
+            "logp_rwd_mean": logp_rwd.cpu().mean().data.item(),
+            "logp_rwd_std": logp_rwd.cpu().std().data.item(),
+            "logp_obs_mean": logp_obs.cpu().mean().data.item(),
+            "logp_obs_std": logp_obs.cpu().std().data.item(),
         }
-        return adv_loss, adv_q_loss, next_obs, done, stats
+        return adv_loss, adv_q_loss, next_obs, stats
     
-    def take_adversarial_model_gradient_step(self, obs, act, done, sl_batch):
+    def take_adversarial_model_gradient_step(self, obs, act, sl_batch):
         self.reward.train()
         self.dynamics.train()
         
-        adv_loss, adv_q_loss, next_obs, done, adv_stats = self.compute_dynamics_adversarial_loss(obs, act, done)
-        reward_loss = self.compute_reward_loss(sl_batch)
-        dynamics_loss = self.compute_dynamics_loss(sl_batch)
+        adv_loss, adv_q_loss, next_obs, adv_stats = self.compute_dynamics_adversarial_loss(obs, act)
+        reward_loss = self.reward.compute_loss(sl_batch["obs"], sl_batch["act"], sl_batch["rwd"])
+        dynamics_loss = self.dynamics.compute_loss(sl_batch["obs"], sl_batch["act"], sl_batch["next_obs"])
         total_loss = reward_loss + dynamics_loss + self.adv_penalty * adv_loss
         total_loss.backward()
         if self.update_critic_adv:
             (self.adv_penalty * adv_q_loss).backward()
         
-        if self.grad_clip is not None:
-            nn.utils.clip_grad_norm_(self.parameters(), self.grad_clip)
+        # if self.grad_clip is not None:
+        #     nn.utils.clip_grad_norm_(self.parameters(), self.grad_clip)
         self.optimizers["reward"].step()
         self.optimizers["dynamics"].step()
         if self.update_critic_adv:
@@ -205,21 +188,21 @@ class RAMBO(MBPO):
         
         self.reward.eval()
         self.dynamics.eval()
-        return stats, next_obs, done
+        return stats, next_obs
     
     def train_adversarial_model_epoch(self, steps, rollout_steps, logger=None):
         # train test split supervised learning data
-        num_total = min(self.batch_size * steps, self.real_buffer.size)
+        num_total = min(int(self.batch_size * steps * (1 + self.eval_ratio)), self.real_buffer.size)
         num_eval = int(self.eval_ratio * num_total)
         data = self.real_buffer.sample(num_total)
 
         # normalize data
-        data["obs"] = normalize(data["obs"], self.obs_mean.cpu(), self.obs_variance.cpu())
-        data["next_obs"] = normalize(data["next_obs"], self.obs_mean.cpu(), self.obs_variance.cpu())
-        data["rwd"] = normalize(data["rwd"], self.rwd_mean.cpu(), self.rwd_variance.cpu())
-        
-        train_data = {k:v[:-num_eval] for k, v in data.items()}
-        eval_data = {k:v[-num_eval:] for k, v in data.items()}
+        data["obs"] = normalize(data["obs"], self.dynamics.obs_mean.cpu(), self.dynamics.obs_variance.cpu())
+        data["next_obs"] = normalize(data["next_obs"], self.dynamics.obs_mean.cpu(), self.dynamics.obs_variance.cpu())
+        data["rwd"] = normalize(data["rwd"], self.reward.out_mean.cpu(), self.reward.out_variance.cpu())
+
+        train_data = {k:v[:-num_eval].to(self.device) for k, v in data.items()}
+        eval_data = {k:v[-num_eval:].to(self.device) for k, v in data.items()}
 
         # shuffle train data
         idx_train = np.arange(len(train_data["obs"]))
@@ -231,15 +214,14 @@ class RAMBO(MBPO):
         while counter < steps:
             batch = self.real_buffer.sample(self.batch_size)
             obs = batch["obs"].to(self.device)
-            done = batch["done"].to(self.device)
             for t in range(rollout_steps):
                 # get supervised batch
                 idx_batch = idx_train[idx_start_sl:idx_start_sl+self.batch_size]
                 train_batch = {k:v[idx_batch] for k, v in train_data.items()}
                 
                 act = self.choose_action(obs)
-                model_train_stats, next_obs, done = self.take_adversarial_model_gradient_step(
-                    obs, act, done, train_batch
+                model_train_stats, next_obs = self.take_adversarial_model_gradient_step(
+                    obs, act, train_batch
                 )
                 train_stats_epoch.append(model_train_stats)
                 obs = next_obs.clone()
@@ -261,14 +243,20 @@ class RAMBO(MBPO):
         train_stats_epoch = pd.DataFrame(train_stats_epoch).mean(0).to_dict()
 
         # evaluate
-        reward_eval_stats = self.eval_reward(eval_data)
-        model_eval_stats = self.eval_model(eval_data)
+        reward_eval_stats = self.reward.evaluate(eval_data["obs"], eval_data["act"], eval_data["rwd"])
+        dynamics_eval_stats = self.dynamics.evaluate(eval_data["obs"], eval_data["act"], eval_data["next_obs"])
+        self.reward.update_topk_dist(reward_eval_stats)
+        self.dynamics.update_topk_dist(dynamics_eval_stats)
+        eval_stats = {
+            "rwd_mae": reward_eval_stats["mae"],
+            "obs_mae": dynamics_eval_stats["mae"],
+        }
         if logger is not None:
-            logger.push({**reward_eval_stats, **model_eval_stats})
-
-        stats_epoch = {**train_stats_epoch, **reward_eval_stats, **model_eval_stats}
+            logger.push(eval_stats)
+        
+        stats_epoch = {**train_stats_epoch, **eval_stats}
         return stats_epoch
-
+    
     def train_policy(
         self, eval_env, max_steps, epochs, steps_per_epoch, sample_model_every, 
         rwd_fn=None, num_eval_eps=0, eval_deterministic=True, callback=None, verbose=10
@@ -282,7 +270,6 @@ class RAMBO(MBPO):
             # train model
             if t == 0 or (t + 1) % sample_model_every == 0:
                 # generate imagined data
-                # self.replay_buffer.clear()
                 rollout_steps = self.compute_rollout_steps(epoch + 1)
                 self.replay_buffer.max_size = min(
                     self.buffer_size, int(self.model_retain_epochs * self.rollout_batch_size * rollout_steps)
@@ -290,7 +277,9 @@ class RAMBO(MBPO):
                 self.sample_imagined_data(
                     self.rollout_batch_size, rollout_steps, mix=False
                 )
-                print("replay buffer size", self.replay_buffer.size, "rollout steps", rollout_steps)
+                print("rollout_steps: {}, real buffer size: {}, fake buffer size: {}".format(
+                    rollout_steps, self.real_buffer.size, self.replay_buffer.size
+                ))
 
             # train policy
             policy_stats_epoch = self.train_policy_epoch(
@@ -303,7 +292,6 @@ class RAMBO(MBPO):
             # train model adversary
             if (t + 1) % steps_per_epoch == 0:
                 model_stats_epoch = self.train_adversarial_model_epoch(self.m_steps, rollout_steps, logger=logger)
-                self.compute_topk_dist(model_stats_epoch)
                 if verbose:
                     round_loss_dict = {k: round(v, 3) for k, v in model_stats_epoch.items()}
                     print(f"e: {epoch + 1}, t model: {t + 1}, {round_loss_dict}")
@@ -320,12 +308,10 @@ class RAMBO(MBPO):
 
                         # compute estimated return 
                         with torch.no_grad():
-                            eval_obs_norm = normalize(
-                                eval_eps[-1]["obs"].to(self.device), self.obs_mean, self.obs_variance
+                            r, _ = self.reward.step(
+                                eval_eps[-1]["obs"].to(self.device),
+                                eval_eps[-1]["act"].to(self.device)
                             )
-                            eval_act = eval_eps[-1]["act"].to(self.device)
-                            r_norm = self.sample_reward_dist(eval_obs_norm, eval_act)
-                            r = denormalize(r_norm, self.rwd_mean, self.rwd_variance)
                         logger.push({"eval_eps_est_return": sum(r.cpu())})
                         logger.push({"eval_eps_return": sum(eval_eps[-1]["rwd"])})
                         logger.push({"eval_eps_len": sum(1 - eval_eps[-1]["done"])})
@@ -336,6 +322,6 @@ class RAMBO(MBPO):
                 print()
 
                 if callback is not None:
-                    callback(self, logger)
+                    callback(self, pd.DataFrame(logger.history))
         
         return logger
